0b8e9b79a0b250c54175de53a6b1a9a1c54bdeb6
abufadda01 <laithfadda4@gmail.com>
2023-11-29 11:04:38 +0300

    cretae paes folder and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
+import {BrowserRouter as Router , Routes , Route } from "react-router-dom"
+import Home from "./pages/Home" 
+import About from "./pages/About" 
+import Profile from "./pages/Profile" 
+import SignIn from "./pages/SignIn" 
+import SignUp from "./pages/SignUp" 
 
 
 function App() {
-
   return (
     <>
-  app app
+      <Router>
+
+        <Routes>
+
+          <Route path="/" element={<Home/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+
+        </Routes>
+
+      </Router>
     </>
   ) 
 }
