e2f46dcdeba2a0f5665fbc3b5fc6502f9a912c3d
unknown <laithfadda4@gmail.com>
2023-12-23 16:22:04 +0300

    complete create estate page

--- client/src/pages/CreateEstate.jsx
+++ client/src/pages/CreateEstate.jsx
@@ -1,19 +1,40 @@
 import React, { useState } from 'react'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage"
 import {app} from "../firebase"
+import axios from 'axios'
+import { useSelector , useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 
 const CreateEstate = () => {
 
     const [files , setFiles] = useState([])
 
+    const navigate = useNavigate()
+
+    const {token , currentUser} = useSelector((state) => state.user)
+
     const [formData , setFormData] = useState({
+        name : "",
+        description : "" ,
+        address : "",
+        regularPrice : 50 ,
+        discountPrice : 20 ,
+        bedrooms : 1 ,
+        bathrooms : 1 ,
+        furnished : false ,
+        parking : false ,
+        offer : false ,
+        typeOfEstate : "sale",
         imageUrls : []
     })
 
+
+
     const [imageUploadError , setImageUploadError] = useState(false)
-    
     const [imageUploded , setImageUploaded] = useState(false)
+    const [error , setError] = useState(false)
+    const [loading , setLoading] = useState(false)
 
 
 
@@ -65,6 +86,22 @@ const CreateEstate = () => {
     
 
 
+    const handleChange = (e) => {
+        // handle all form data type changes (text , numbers , checkbox , etc...)
+        if(e.target.name === "sale" || e.target.name === "rent"){
+            setFormData({...formData , typeOfEstate : e.target.name})
+        }
+        if(e.target.name === "parking" || e.target.name === "furnished" || e.target.name === "offer"){
+            setFormData({...formData , [e.target.name] : e.target.checked})
+        }
+        if(e.target.type === "number" || e.target.type === "text" || e.target.type === "textarea"){
+            setFormData({...formData , [e.target.name] : e.target.value})
+        }
+        
+    }
+
+
+
     const storeImage = async (file) => {
 
         return new Promise((resolve , reject) => {
@@ -91,47 +128,80 @@ const CreateEstate = () => {
 
 
 
+    const handleSubmit = async (e) => {
+        
+        e.preventDefault()
+
+        setLoading(true)
+        setError(false)
+
+        if(formData.imageUrls.length < 1) return setError("you must upload at least one image")
+        if(formData.regularPrice < formData.discountPrice) return setError("Discount price must be lower than regular price")
+
+        try {
+
+            const response = await axios.post("/api/estate/createEstate" , {
+                ...formData ,
+                user : currentUser._id
+            } , {
+                headers : {
+                    "access_token" : token
+                }
+            })
+
+            setLoading(false)
+            navigate(`/estate/${response.data._id}`)
+            
+
+        } catch (error) {
+            setError(error.message)
+            setLoading(false)
+        }
+    }
+
+
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
 
         <h1 className='text-4xl text-center font-semibold mt-7 mb-10'>Create Estate</h1>
 
         {/* // display will be a column , then on bigger screens (big than small) will become row */}
-        <form className='flex flex-col sm:flex-row gap-7'>
+        <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-7'>
             
             {/* all inputs div */}
             <div className='flex flex-col gap-4 flex-1'>
                 
                 {/* text inputs */}
-                <input type="text" placeholder='estate name' className='border p-3 rounded-lg' name='estateName' maxLength={62} minLength={10} required />
-                <textarea type="text" placeholder='Description' className='border p-3 rounded-lg' name='description' required />
-                <input type="text" placeholder='Address' className='border p-3 rounded-lg' name='address' required />
+                <input onChange={handleChange} value={formData.name} type="text" placeholder='estate name' className='border p-3 rounded-lg' name='name' maxLength={62} minLength={10} required />
+                <textarea onChange={handleChange} value={formData.description} type="text" placeholder='Description' className='border p-3 rounded-lg' name='description' required />
+                <input type="text" onChange={handleChange} value={formData.address} placeholder='Address' className='border p-3 rounded-lg' name='address' required />
                 
                 {/* check box inputs div */}
                 <div className='flex gap-6 flex-wrap mt-4'>
 
                     <div className='flex gap-2'>
-                        <input type="checkbox" name='sale' className='w-5' />
+                        <input type="checkbox" name='sale' className='w-5' onChange={handleChange} checked={formData.typeOfEstate === "sale"} />
                         <span>Sell</span>
                     </div>
 
                     <div className='flex gap-2'>
-                        <input type="checkbox" name='parking' className='w-5' />
+                        <input type="checkbox" name='rent' className='w-5' onChange={handleChange} checked={formData.typeOfEstate === "rent"} />
+                        <span>Rent</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <input type="checkbox" name='parking' className='w-5' onChange={handleChange} checked={formData.parking} />
                         <span>Parking spot</span>
                     </div>
 
                     <div className='flex gap-2'>
-                        <input type="checkbox" name='furnished' className='w-5' />
+                        <input type="checkbox" name='furnished' className='w-5' onChange={handleChange} checked={formData.furnished} />
                         <span>Furnished</span>
                     </div>
 
                     <div className='flex gap-2'>
-                        <input type="checkbox" name='furnished' className='w-5' />
-                        <span>Furnished</span>
-                    </div>
-
-                    <div className='flex gap-2'>
-                        <input type="checkbox" name='offer' className='w-5' />
+                        <input type="checkbox" name='offer' className='w-5' onChange={handleChange} value={formData.offer} />
                         <span>Offer</span>
                     </div>
 
@@ -141,34 +211,41 @@ const CreateEstate = () => {
                 <div className='flex flex-wrap gap-6 mt-4'>
 
                     <div className='flex items-center gap-2'>
-                        <input type="number" name='bedrooms' className='p-3 rounded-lg border-gray-300' min={1} max={10} required />
+                        <input onChange={handleChange} value={formData.bedrooms} type="number" name='bedrooms' className='p-3 rounded-lg border-gray-300' min={1} max={1000} required />
                         <p>Bedrooms</p>
                     </div>
 
                     <div className='flex items-center  gap-2'>
-                        <input type="number" name='bathrooms' className='p-3 rounded-lg border-gray-300' min={1} max={10} required />
+                        <input onChange={handleChange} value={formData.bathrooms} type="number" name='bathrooms' className='p-3 rounded-lg border-gray-300' min={1} max={1000} required />
                         <p>Bathrooms</p>
                     </div>
 
                     <div className='flex items-center gap-2'>
-                        <input type="number" name='regularPrice' className='p-3 rounded-lg border-gray-300' min={1} max={10} required />
+
+                        <input onChange={handleChange} min={50} max={100000} value={formData.regularPrice} type="number" name='regularPrice' className='p-3 rounded-lg border-gray-300'  required />
                         
                         <div className='flex flex-col items-start'>
                             <p>Regular Price</p>
-                            <span className='text-xs'>($ / month)</span>
+                            {formData.typeOfEstate === "rent" && <span className='text-xs'>($ / month)</span>}
                         </div>
 
                     </div>
 
-                    <div className='flex items-center gap-2'>
-                        <input type="number" name='discountPrice' className='p-3 rounded-lg border-gray-300' min={1} max={10} required />
+                    {formData.offer && 
+                    (
+                        <div className='flex items-center gap-2'>
+
+                        <input onChange={handleChange} min={formData.discountPrice} max={100000} value={formData.discountPrice} type="number" name='discountPrice' className='p-3 rounded-lg border-gray-300' required />
                         
                         <div className='flex flex-col items-start'>
                             <p>Discount Price</p>
-                            <span className='text-xs'>($ / month)</span>
+                            {formData.typeOfEstate === "rent" && <span className='text-xs'>($ / month)</span>}
                         </div>
 
                     </div>
+                    )
+                    }
+                    
 
                 </div>
 
@@ -188,7 +265,9 @@ const CreateEstate = () => {
                     <button type='button' disabled={imageUploded} onClick={handleImagesUpload} className='capitalize p-2 border bg-green-700 border-green-700 text-white  rounded hover:shadow-lg hover:bg-white hover:text-green-700 transition-all duration-150 ease-in disabled:opacity-80'>{imageUploded ? "Uploading..." : "upload"}</button>
                 </div>
 
-                <button className='p-3 rounded-lg bg-slate-700 text-white capitalize hover:opacity-90 disabled:opacity-80'>Create Estate</button>
+                <button disabled={loading || imageUploded} className='p-3 rounded-lg bg-slate-700 text-white capitalize hover:opacity-90 disabled:opacity-80'>{loading ? "Creating..." : "Create Estate"}</button>
+                
+                {error && <p className='text-red-700 text-sm font-semibold'>{error}</p>}
             
                 <p className='text-red-600 font-semibold'>{imageUploadError && imageUploadError}</p>
             
